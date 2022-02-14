f712d84630e2bdf74f87869b5e390e98aa21e143
marcelocassar <marcelocassar@gmail.com>
2022-02-14 16:04:52 -0300

    dsa

---
+++ Guanabara/Aula11/Ex008.js
@@ -0,0 +1,11 @@
+console.log('O console funcionou corretamente')
+
+// apertar a tecla f8 e rodar o note exec
+var vel = 220.5
+
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+if (vel > 60){ //condição simples, pq não tem senao, só tem o SE(if)
+    console.log(`Você foi multado, sua velocidade foi registrada em: ${vel}`)
+}
+
