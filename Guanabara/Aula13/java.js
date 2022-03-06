b8b67005d9e58437a1baeae9d5cf896e88ef8d65
marcelocassar <marcelocassar@gmail.com>
2022-03-06 20:39:43 -0300

    fds

--- Guanabara/Aula13/java.js
+++ Guanabara/Aula13/java.js
@@ -1,11 +1,54 @@
-function funcao(){
+function verificar(){
 
-var idade = window.document.getElementById('data')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
 
-if (idade > 18) {
+    if (fano.value.length == 0 || fano.value > ano){
+        
+        window.alert("Erro - Verifique os dados.")
 
-    document.write("É um homem maior de 18 anos.")
+    } else {
 
-}
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')        
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                //crianca
+                img.setAttribute('src', 'dia.jpg')
+            } else if (idade >= 10 && idade <18){
+                //jovem
+                img.setAtribute('src', 'noite.jpg')
+            } else if (idade < 65){
+                //adulto
+                img.setAttribute('src', 'tarde.jpg')
+            } else {
+                // idoso
+            }
 
-}
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                //crianca
+                img.setAttribute('src', 'dia.jpg')
+            } else if (idade >= 10 && idade <18){
+                //jovem
+                img.setAtribute('src', 'noite.jpg')
+            } else if (idade < 65){
+                //adulto
+                img.setAttribute('src', 'tarde.jpg')
+            } else {
+                // idoso
+            }
+                
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = "<p>Detectamos :" + genero +" e idade: " + idade + "</p>"
+        res.appendChild(img)
+    }
+    }
